3db51a049f81811ea1b232e967fc8b6ceee2f1cc
maximeRoudier <36051892+maximeRoudier@users.noreply.github.com>
2019-02-18 23:39:58 +0100

    first add

---
+++ sources/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    // variables
+    var $window = $(window),
+        $body = $('body'),
+        $travaux = $('.travaux');
+
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+
+    $travaux.each(function () {
+      var $this = $(this);
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+        $body.removeClass(function (index, css) {
+          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+        });
+        $body.addClass('color-' + $(this).data('color'));
+      }
+    });
+  }).scroll();
+
+
+  $('.anchor-scroll').anchorScroll({
+     scrollSpeed: 800, // scroll speed
+     offsetTop: 0,
+     onScroll: function () {
+     },
+     scrollEnd: function () {
+     }
+  });
+});
